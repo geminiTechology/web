9b3d02ee0da2ef95c0912fafca1a466cd83c4e2f
r.sluka <robosluka11@gmail.com>
2025-11-02 11:36:20 +0100

    Add EmailJS setup and .env example 2

--- src/App.tsx
+++ src/App.tsx
@@ -3,28 +3,22 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { LanguageProvider } from './contexts/LanguageContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import BotProtection from './components/BotProtection';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import CourseDetail from './pages/CourseDetail';
 import About from './pages/About';
 import Contact from './pages/Contact';
-import { checkRefreshLimit } from './utils/botProtection';
 import './App.css';
 
 function App() {
   const [isBlocked, setIsBlocked] = useState(false);
 
-  useEffect(() => {
-    const protectionCheck = checkRefreshLimit();
-    setIsBlocked(!protectionCheck.allowed);
-  }, []);
+
 
   return (
     <LanguageProvider>
       <Router>
         <div className="App">
-          <BotProtection />
           {!isBlocked && (
             <>
               <Header />
